65f1b9e0e3166dd10c35df96cba5acc89e29ad51
LazyNoodles <144541228+LazyNoodles@users.noreply.github.com>
2024-10-28 17:30:18 +0700

    Finalized the JS script and CSS changes

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,78 +1,111 @@
 const options = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let isGameOver = false;
+let scoreToReach = 5;
+
+
 
 function getComputerChoice(){
-    return computerTurn = options[Math.floor(Math.random() * options.length)];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-function getHumanChoice(){
-    let validInput = false;
-    while (validInput == false){
-        const choice = prompt("Rock Paper Scissors");
-        if (choice == null){
-            continue;
-        }
-        const choiceLower = choice.toLowerCase();
-        if (options.includes(choiceLower)){
-            validInput = true;
-            return choiceLower;
-        }
-    }
-}
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const newGame = document.querySelector("#playAgain");
+const finalScore = document.querySelector("#final");
+const roundScore = document.querySelector("#roundScore");
 
-function checkWinner(humanSelection, computerSelection){
-    if (humanSelection == computerSelection){
-        return "Tie";
+rockBtn.addEventListener("click", function(){
+    if (!isGameOver){
+    playGame("rock", getComputerChoice());
+    endGame();
     }
-    else if (
-        (humanSelection == "rock" && computerSelection == "scissors") ||
-        (humanSelection == "paper" && computerSelection == "rock") ||
-        (humanSelection == "scissors" && computerSelection == "paper")
-    ) {
-        return "Human";
-    }
-    else {
-        return "Computer";
-    }
-}
+});
 
-function playRound(humanSelection, computerSelection){
-   const result = checkWinner(humanSelection, computerSelection);
-   if (result == "Tie"){
-    return "It is a Tie!";
-   }
-   else if (result == "Human"){
-    return `You Win! ${humanSelection} beats ${computerSelection}`;
-   }
-   else {
-    return `You Loose! ${computerSelection} beats ${humanSelection}`;
-   }
-}
+paperBtn.addEventListener("click", function(){
+    if (!isGameOver){
+    playGame("paper", getComputerChoice());
+    endGame();
+    }
+});
+scissorsBtn.addEventListener("click", function(){
+    if (!isGameOver){
+    playGame("scissors", getComputerChoice());
+    endGame();
+    }
+});
 
-function game(){
-    let humanScore = 0;
-    let computerScore = 0;
-    console.log("Welcome to the Game!");
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-        if (checkWinner(humanSelection, computerSelection) == "Human"){
-            humanScore++;
-        }
-        else if (checkWinner(humanSelection, computerSelection) == "Computer"){
+newGame.addEventListener("click", function(){
+    isGameOver = false;
+    humanScore = 0;
+    computerScore = 0;
+    roundScore.innerText = "";
+    finalScore.innerText = "";
+    human.innerText = "0";
+    computer.innerText = "0";
+})
+
+const computer = document.querySelector("#computer");
+const human = document.querySelector("#human");
+
+// Determine who wins each round using the game's logic
+function playGame(humanSelection, computerSelection){
+    if (humanSelection === computerSelection){
+        roundScore.innerText= "It is a Tie!";
+    }
+    else if (humanSelection === "rock"){
+        if (computerSelection === "paper"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You lost.`;
             computerScore++;
+            computer.innerText = computerScore;
+        }
+        else if (computerSelection === "scissors"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You win!`;
+            humanScore++;
+            human.innerText = humanScore;
         }
     }
-    console.log("Game is Finished!");
-    if (humanScore > computerScore){
-        console.log("Human was the Winner!");
+    else if (humanSelection === "paper"){
+        if (computerSelection === "rock"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You win!`;
+            humanScore++;
+            human.innerText = humanScore;
+        }
+        else if (computerSelection === "scissors"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You lost.`;
+            computerScore++;
+            computer.innerText = computerScore;
+        }
     }
-    else if (humanScore < computerScore){
-        console.log("Computer was the Winner!");
-    }
-    else{
-        console.log("Tie!");
+    else if (humanSelection === "scissors"){
+        if (computerSelection === "paper"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You win!`;
+            humanScore++;
+            human.innerText = humanScore;
+        }
+        else if (computerSelection === "rock"){
+            roundScore.innerText = `You chose ${humanSelection} and the computer chose ${computerSelection}. You lost.`;
+            computerScore++;
+            computer.innerText = computerScore;
+        }
     }
 }
 
-game();
+// Determine who has reached the desired goal first and end the game
+function endGame() {
+    if (humanScore === scoreToReach || computerScore === scoreToReach) {
+        isGameOver = true;
+        if (humanScore === scoreToReach) {
+        finalScore.innerText = "You win!";
+        human.classList.add("winner");
+        computer.classList.add("loser");
+    } else if (computerScore === scoreToReach){
+    finalScore.innerText = "Computer wins!";
+    human.classList.add("loser");
+    computer.classList.add("winner");
+    }
+}}
+
+
